a5c5985b882931b28b3cb89936fcfbfa08d22182
Selhar <selhar@protonmail.com>
2019-07-24 20:17:09 -0300

    feat: initial project clean up.

---
+++ src/styleguide/colors.js
@@ -0,0 +1,11 @@
+// nomenclature
+export const gigas = "#553C8B";
+export const gallery = "#EAEAEA";
+
+// descriptive
+export const darkPurple = gigas;
+export const milkyGrey = gallery;
+
+// functional
+export const primary = darkPurple;
+export const secondary = gallery;
